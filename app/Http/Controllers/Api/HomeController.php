475e60fe460a2d636567f0aefb95b98aae9a5fe1
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-05-27 12:45:39 +0300

    firat commit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -87,11 +87,12 @@ public function getTime(Request $request)
         return response()->json(['time_slots' => []]); // No time slots for this date & addon
     }
 
-return response()->json([
+return $this->success([
     'time_slots' => $bookingDate->timeSlots->pluck('time')->map(function ($time) {
-        return Carbon::parse($time)->format('h:i A'); // e.g., 11:16 AM or 12:16 AM
-    })
+        return Carbon::parse($time)->format('h:i A');
+    }),
 ]);
+
 }
 
     public function getwhyus()
