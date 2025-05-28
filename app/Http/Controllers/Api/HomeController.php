1abffcf3ce6ca329a390b79597124f4f6b6119c9
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-05-28 16:11:26 +0300

    firat commit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -88,15 +88,15 @@ public function getTime(Request $request)
         ->where('day_date', $request->day_date)
         ->first();
 
-    if (!$bookingDate || !$bookingDate->is_available) {
-        return $this->success('', [
-            'time_slots' => [],
-        ]);
+    if (!$bookingDate) {
+        return $this->success('', []);
     }
 
     $timeSlots = $bookingDate->timeSlots->pluck('time')->mapWithKeys(function ($time) {
-        $key = Carbon::createFromFormat('H:i:s', $time)->format('H:i'); // key = "14:30"
-        $value = Carbon::createFromFormat('H:i:s', $time)->format('h:i A'); // value = "02:30 PM"
+        // Format the key as HH:mm (e.g. 14:30)
+        $key = Carbon::createFromFormat('H:i:s', $time)->format('H:i');
+        // Format the value as 12-hour time with AM/PM (e.g. 02:30 PM)
+        $value = Carbon::createFromFormat('H:i:s', $time)->format('h:i A');
         return [$key => $value];
     });
 
